17c707bc9f0a42617d407b5da60a048b89075282
KenJend0 <teyssir.aissi@edu.devinci.fr>
2025-01-06 14:46:06 +0100

    Commit

--- fronten/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import './index.css'; // Assurez-vous que ce fichier existe, sinon supprimez cette ligne
-
-const container = document.getElementById('root');
-const root = createRoot(container); // Créez un "root" avec React 18
-root.render(<App />);
